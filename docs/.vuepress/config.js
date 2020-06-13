63bbda9f88019786bcbdd7fdfdd734712c57f49a
jihua <1164258202@qq.com>
2020-06-13 23:43:51 +0800

    Feat: 修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     base: '/swiftdo/',
-    title: 'Swift',
+    title: 'Oldbirds',
     description: 'Swift learning',
     head: [
         ['link', {
@@ -8,12 +8,73 @@ module.exports = {
             href: '/favicon.ico'
         }],
     ],
-    themeConfig: {
-        logo: 'logo.png'
-    },
-    plugins: ['@vuepress/back-to-top',
+
+    plugins: [
+        '@vuepress/back-to-top',
         '@vuepress/active-header-links',
         '@vuepress/nprogress',
         '@vuepress/medium-zoom',
+    ],
+
+    themeConfig: {
+        logo: 'logo.png',
+        repo: 'swiftdo/swiftdo',
+        editLinks: true,
+        smoothScroll: true,
+        lastUpdated: '上次更新',
+        nav: [{
+                text: 'Swift',
+                link: '/swift/'
+            },
+            {
+                text: 'Flutter',
+                link: '/flutter/'
+            },
+            {
+                text: 'Figma',
+                link: '/figma/'
+            },
+            {
+                text: '工具',
+                link: '/tools/'
+            },
+            {
+                text: '关于',
+                link: '/about.html'
+            }
+        ],
+
+        sidebarDepath: 2,
+        sidebar: {
+            '/swift/': getSwiftSidebar(),
+            '/flutter/': getFlutterSidebar(),
+            '/figma/': getFigmaSidebar(),
+            '/tools/': getToolsSidebar(),
+        }
+    },
+}
+
+function getSwiftSidebar() {
+    return [
+        'swift',
+        'swiftui'
+    ]
+}
+
+function getFlutterSidebar() {
+    return [
+        'flutter'
+    ]
+}
+
+function getFigmaSidebar() {
+    return [
+        'figma'
+    ]
+}
+
+function getToolsSidebar() {
+    return [
+        'tools'
     ]
 }
