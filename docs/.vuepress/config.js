5d3b7004d9f56114b44cdd18f084e0ce2580945f
jihua <1164258202@qq.com>
2020-06-13 22:00:49 +0800

    Feat: 新增插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,16 @@
 module.exports = {
+    base: '/swiftdo/',
     title: 'Swift',
-    description: 'Swift learning'
+    description: 'Swift learning',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: 'favicon.icon'
+        }],
+    ],
+    plugins: ['@vuepress/back-to-top',
+        '@vuepress/active-header-links',
+        '@vuepress/nprogress',
+        '@vuepress/medium-zoom',
+    ]
 }
