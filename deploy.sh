290b51da3a788e2e27958ac1adb12d854d3e8305
jihua <1164258202@qq.com>
2020-06-14 12:35:37 +0800

    CI: 脚本修改

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,7 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:swiftdo/swiftdo.github.io.git master
 
+
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:swiftdo/swiftdo.git master:gh-pages
 
