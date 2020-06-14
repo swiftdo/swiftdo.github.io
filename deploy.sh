6b98b9867b7f4bf42349b63cc2067cdb68b9857f
jihua <1164258202@qq.com>
2020-06-14 12:06:40 +0800

    Feat：修改

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:swiftdo/swiftdo.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:swiftdo/swiftdo.git master:gh-pages
+# git push -f git@github.com:swiftdo/swiftdo.git master:gh-pages
 
 cd -
