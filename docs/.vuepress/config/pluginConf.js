3932cc22e2645f284b87d1b28bf6e172f7f5b1cb
laijihua <1164258202@qq.com>
2020-08-20 08:55:34 +0800

    Feat: 添加 robots 插件

--- docs/.vuepress/config/pluginConf.js
+++ docs/.vuepress/config/pluginConf.js
@@ -44,5 +44,27 @@ module.exports = {
             content: "复制成功!"
         }
     },
-    'vuepress-plugin-baidu-autopush': {}
+    'vuepress-plugin-baidu-autopush': {},
+    'robots': {
+        /**
+         * @host
+         * Mandatory, You have to provide the host URL
+         */   
+        host: "https://oldbird.run",
+        /**
+         * @disallowAll
+         * Optional: if it's true, all others options are ignored and exclude all robots from the entire server
+         */
+        disallowAll: false,
+        /**
+         * @allowAll
+         * Optional: if it's true and @disallowAll is false, all others options are ignored and allow all robots complete access
+         */
+        allowAll: true,      
+        /**
+         * @sitemap
+         * Optional, by default: sitemap.xml
+         */ 
+        sitemap: "/sitemap.xml",
+    }
 }
