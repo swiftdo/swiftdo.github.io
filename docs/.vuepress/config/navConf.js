27f7b28fa9a50d587bde43d95a29ef36a29e25a8
lai <1164258202@qq.com>
2021-06-17 14:43:57 +0800

    Feat: add

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -1,98 +1,98 @@
 module.exports = [{
-        text: 'Swift',
-        items: [{
-                text: 'SwiftUI',
-                link: '/swift/swiftui/'
-            },
-            {
-                text: 'Vapor',
-                link: '/swift/vapor/'
-            },
-            {
-                text: 'iOS',
-                link: '/swift/ios/'
-            },
-            {
-                text: 'Tips',
-                link: '/swift/tips/'
-            },
-            {
-                text: '函数式编程',
-                link: '/swift/fp/'
+    text: 'Swift',
+    items: [{
+        text: 'SwiftUI',
+        link: 'https://oldbird.run/swiftui/#/'
+    },
+    {
+        text: 'Vapor',
+        link: '/swift/vapor/'
+    },
+    {
+        text: 'iOS',
+        link: '/swift/ios/'
+    },
+    {
+        text: 'Tips',
+        link: '/swift/tips/'
+    },
+    {
+        text: '函数式编程',
+        link: '/swift/fp/'
 
-            }
-        ]
-    },
-    {
-        text: 'Haskell',
-        link: '/haskell/'
-    },
-    {
-        text: 'Flutter',
-        link: '/flutter/'
-    },
-    {
-        text: 'AI',
-        link: '/ai/'
-    },
-    {
-        text: 'Figma',
-        link: '/figma/'
-    },
-    {
-        text: '基础',
-        items: [{
-            text: '概要',
-            link: '/basis/index/'
-        },{
-            text: '数据结构与算法',
-            link: '/basis/algorithms/'
-        }, {
-            text: '操作系统',
-            link: '/basis/os/'
-        }, {
-            text: '设计模式',
-            link: 'https://oldbird.run/design-patterns/#/'
-        }]
-    },
-    {
-        text: '工具',
-        items: [{
-                text: 'VuePress',
-                link: '/tools/vuepress/'
-            },
-            {
-                text: 'Git',
-                link: '/tools/git/'
-            },
-            {
-                text: 'YAML',
-                link: '/tools/yaml/'
-            },
-            {
-                text: 'HTML',
-                link: '/tools/html/'
-            },
-            {
-                text: 'PlantUML',
-                link: '/tools/plantuml/'
-            }
-        ]
-    },
-    {
-        text: '书单',
-        link: '/book/'
-    },
-    {
-        text: '关于',
-        items: [{
-                text: '关于我们',
-                link: '/about/index/'
-            },
-            {
-                text: 'APP',
-                link: '/about/app/'
-            }
-        ]
     }
+    ]
+},
+{
+    text: 'Haskell',
+    link: '/haskell/'
+},
+{
+    text: 'Flutter',
+    link: '/flutter/'
+},
+{
+    text: 'AI',
+    link: '/ai/'
+},
+{
+    text: 'Figma',
+    link: '/figma/'
+},
+{
+    text: '基础',
+    items: [{
+        text: '概要',
+        link: '/basis/index/'
+    }, {
+        text: '数据结构与算法',
+        link: '/basis/algorithms/'
+    }, {
+        text: '操作系统',
+        link: '/basis/os/'
+    }, {
+        text: '设计模式',
+        link: 'https://oldbird.run/design-patterns/#/'
+    }]
+},
+{
+    text: '工具',
+    items: [{
+        text: 'VuePress',
+        link: '/tools/vuepress/'
+    },
+    {
+        text: 'Git',
+        link: '/tools/git/'
+    },
+    {
+        text: 'YAML',
+        link: '/tools/yaml/'
+    },
+    {
+        text: 'HTML',
+        link: '/tools/html/'
+    },
+    {
+        text: 'PlantUML',
+        link: '/tools/plantuml/'
+    }
+    ]
+},
+{
+    text: '书单',
+    link: '/book/'
+},
+{
+    text: '关于',
+    items: [{
+        text: '关于我们',
+        link: '/about/index/'
+    },
+    {
+        text: 'APP',
+        link: '/about/app/'
+    }
+    ]
+}
 ]
