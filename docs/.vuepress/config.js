4f6618b5723176119afb68eec67d5575832f4a9a
laijihua <1164258202@qq.com>
2022-03-19 21:54:04 +0800

    开启评价

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -74,13 +74,13 @@ module.exports = {
       flowchart: true,
     },
 
-    // vssueConfig: {
-    //     platform: 'github',
-    //     owner: 'swiftdo',
-    //     repo: 'swiftdo.github.io',
-    //     clientId: 'c91ff3d0b5753a4788de',
-    //     clientSecret: 'ad67e4630d3bf3709badbfcfe6c5dfdc21d4c04a',
-    //     autoCreateIssue: true
-    // }
+    vssueConfig: {
+        platform: 'github',
+        owner: 'swiftdo',
+        repo: 'swiftdo.github.io',
+        clientId: 'c91ff3d0b5753a4788de',
+        clientSecret: 'ad67e4630d3bf3709badbfcfe6c5dfdc21d4c04a',
+        autoCreateIssue: true
+    }
   },
 };
