d98c3960ecd59ca0483cc8d126340f33a593ca12
laijihua <1164258202@qq.com>
2022-05-07 21:03:33 +0800

    detail

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -42,71 +42,72 @@ module.exports = [
     text: "Python",
     link: "/python/",
   },
-  
   {
-    text: "基础",
+    text:"其他",
     items: [
       {
-        text: "概要",
-        link: "/basis/index/",
+        text: "基础",
+        items: [
+          {
+            text: "概要",
+            link: "/basis/index/",
+          },
+          {
+            text: "数据结构与算法",
+            link: "/basis/algorithms/",
+          },
+          {
+            text: "操作系统",
+            link: "/basis/os/",
+          },
+        ],
       },
       {
-        text: "数据结构与算法",
-        link: "/basis/algorithms/",
-      },
-      {
-        text: "操作系统",
-        link: "/basis/os/",
-      },
-      {
-        text: "设计模式",
-        link: "/basis/design-patterns/",
-      },
-    ],
-  },
-  {
-    text: "工具",
-    items: [
-      {
-        text: "Git",
-        link: "/tools/git/",
-      },
-      {
-        text: "YAML",
-        link: "/tools/yaml/",
-      },
-      {
-        text: "HTML",
-        link: "/tools/html/",
-      },
-      {
-        text: "PlantUML",
-        link: "/tools/plantuml/",
-      },
-      {
-        text: "流程图 Flowchart",
-        link: "/tools/flowchart/",
-      },
-      {
-        text: "AI",
-        link: "/ai/",
-      },
-      {
-        text: "Figma",
-        link: "/figma/",
-      },
-      {
-        text: "VuePress",
-        link: "/tools/vuepress/",
-      },
-      {
-        text: "Note",
-        link: "/tools/note/",
+        text: "工具",
+        items: [
+          {
+            text: "Git",
+            link: "/tools/git/",
+          },
+          {
+            text: "YAML",
+            link: "/tools/yaml/",
+          },
+          {
+            text: "HTML",
+            link: "/tools/html/",
+          },
+          {
+            text: "PlantUML",
+            link: "/tools/plantuml/",
+          },
+          {
+            text: "流程图 Flowchart",
+            link: "/tools/flowchart/",
+          },
+          {
+            text: "AI",
+            link: "/ai/",
+          },
+          {
+            text: "Figma",
+            link: "/figma/",
+          },
+          {
+            text: "VuePress",
+            link: "/tools/vuepress/",
+          },
+          {
+            text: "Note",
+            link: "/tools/note/",
+          },
+        ],
       },
       {
         text: "Haskell",
         link: "/haskell/",
       },
+
       {
         text: "C&C++",
         items: [
@@ -124,7 +125,13 @@ module.exports = [
           },
         ],
       },
-    ],
+    ]
+  },
+  
+  
+  {
+    text: "设计模式",
+    link: "/basis/design-patterns/",
   },
   {
     text: "书单",
